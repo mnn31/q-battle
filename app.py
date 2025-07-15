396c9c0fa1ee74b4857d0654b6a71914a2d3f049
rohankai <152823439+rohankai@users.noreply.github.com>
2025-07-15 14:57:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,23 @@ from flask import Flask, jsonify
 from characters.bitzy.quantum_move import quantum_move_bitzy
 from characters.bitzy.classical_move import classical_move_bitzy, quirk_bitzy
 from routes import game_api  
+<<<<<<< main
+from characters.bitzy.quantum_move import quantum_move_bitzy
+from characters.bitzy.classical_move import classical_move_bitzy, quirk_bitzy
+from routes import game_api  
 
+app = Flask(__name__)
+app.register_blueprint(game_api)
+=======
+from characters.bitzy.quantum_move import (
+    quantum_move_bitzy_q_thunder,
+    quantum_move_bitzy_shock,
+    quantum_move_bitzy_dualize,
+    quantum_move_bitzy_bit_flip,
+    BitzyQuantumState
+)
+from characters.bitzy.ability import ability_superhijack
+>>>>>>> main
 app = Flask(__name__)
 app.register_blueprint(game_api)
 
